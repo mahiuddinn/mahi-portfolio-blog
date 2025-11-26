133c62bc2b75dc48f80d62b592ccbf8298c38ba9
Mahi <30448348+mahiuddinn@users.noreply.github.com>
2025-11-26 23:34:27 +0600

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,9 @@
+module.exports = function(eleventyConfig) {
+eleventyConfig.addPassthroughCopy("robots.txt"); // copies root file
+eleventyConfig.addPassthroughCopy("public"); // copies everything in /public
+return { dir: { input: ".", output: "_site" } };
+};
+
 const yaml = require("js-yaml");
 const { DateTime } = require("luxon");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
